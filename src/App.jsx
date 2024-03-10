b3e7a9b10d297bb28b8ef5fe7129345a69448718
“[Muhammad <umer.akramk15@gmail.com>
2024-03-10 13:31:54 +0500

    Porject_copleted

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TicTacToe from './components/TicTacToe/TicTacToe'
+
+function App() {
+  return (
+    <div>
+      <TicTacToe></TicTacToe>
+    </div>
+  )
+}
+
+export default App
